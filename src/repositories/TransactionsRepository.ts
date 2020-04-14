1d242fdba46cc999869ca2a2e8925987f5310e53
Fabio Niglio <fabioniglio@gmail.com>
2020-04-14 16:20:07 +0200

    adding routes to post and get transactions

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -15,14 +21,44 @@ class TransactionsRepository {
 
   public all(): Transaction[] {
     // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
     // TODO
+    let income = 0;
+    let outcome = 0;
+    const allTransactions = this.transactions;
+
+    function calculate(type: 'income' | 'outcome', value: number): null {
+      if (type === 'income') {
+        income += value;
+      } else {
+        outcome += value;
+      }
+      return null;
+    }
+    allTransactions.forEach(item => calculate(item.type, item.value));
+
+    const balance = {
+      income,
+      outcome,
+      total: income - outcome,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
     // TODO
+    const transaction = new Transaction({ title, value, type });
+    const { total } = this.getBalance();
+    if (type === 'outcome' && total - value < 0) {
+      throw Error('string');
+    }
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
